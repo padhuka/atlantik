d8fe0f13510f2da3662fd08b9f396d2f77cc5b12
root <root@webserv.bintang-group.co.id>
2019-07-31 08:27:37 +0700

    fix tittle on the web

--- lib/setting.php
+++ lib/setting.php
@@ -1,14 +1,14 @@
 <?php 
 	// DATA BASE
 	$user="root"; // userid untuk akses database anda
-	$db="bodyrepairdb"; // nama database anda.
-	$passsw=""; // password untuk akses database anda
+	$db="atlanticdb"; // nama database anda.
+	$passsw="s0l0b4ru"; // password untuk akses database anda
 	// baris dibawah ini jangan diedit
 	$host="localhost";
 
 	//WEB
-	$title="Gemilang - Cat & Body";
-	$kabupaten="Gemilang";
-	$titlefooter="Apikasi Cat & Body";
+	$title="Atlantic - Cat & Body";
+	$kabupaten="Atlantic";
+	$titlefooter="Aplikasi Atlantic Body Repair & Car Wash v.1";
 
 ?>
--- pages/header_menu.php
+++ pages/header_menu.php
@@ -7,7 +7,7 @@
     <nav class="navbar fixed-top" style="margin-left: 0px">
       <!-- Sidebar toggle button-->
     <div class="container">
-      <a class="navbar-brand" href="index.php" style="color: #ccc">GEMILANG REPAIR
+      <a class="navbar-brand" href="index.php" style="color: #ccc">ATLANTIC
         </a>
       <div class="navbar-custom-menu" style="padding: 0 0 0 0;margin: 0 auto;">
         <ul class="nav navbar-nav" style="list-style: none;padding: 0;margin: 0 auto;font-size: 1px;margin-top: 8px;">
